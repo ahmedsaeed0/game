1ead74e5de469b5c9ece27fc5c347050f0a0b540
ahmedsaeed0 <ahmedsaeed2000.zkh@gmail.com>
2024-11-04 23:06:00 +0300

    linkllddd

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
 import { binanceLogo, dollarCoin, mainCharacter } from './images';
 import Info from './icons/Info';
-import Mine from './icons/Mine';
 import Friends from './icons/Friends';
 import Coins from './icons/Coins';
 
